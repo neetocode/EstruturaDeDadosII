905babbac1b1fd6cc14ccc1142e7a3e13ae1afd4
Miguel Neto <miguelrsn@gmail.com>
2016-10-13 21:23:43 -0300

    tesd

---
+++ ArvoreBinaria/Arvore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArvoreBinaria
+{
+    abstract class Arvore<T>
+    {
+        public No<T> Raiz { get; set; }
+    }
+}
---
+++ ArvoreBinaria/No.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArvoreBinaria
+{
+    class No<T>
+    {
+        public T Valor { get; set; }
+        public No<T> Esquerdo { get; set; }
+        public No<T> Direito { get; set; }
+    }
+}
