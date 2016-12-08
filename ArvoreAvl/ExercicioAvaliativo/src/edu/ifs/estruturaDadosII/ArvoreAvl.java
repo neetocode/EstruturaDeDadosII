66db2ab3077edee1c10e5fd528a8b4410289e856
Miguel Neto <miguelrsn@gmail.com>
2016-12-08 00:18:36 -0300

    okok

--- ArvoreAvl/ExercicioAvaliativo/src/edu/ifs/estruturaDadosII/ArvoreAvl.java
+++ ArvoreAvl/ExercicioAvaliativo/src/edu/ifs/estruturaDadosII/ArvoreAvl.java
@@ -3,12 +3,12 @@ package edu.ifs.estruturaDadosII;
 public class ArvoreAvl {
     private No raiz;
 
-    public void inserir(int valor) {
+    // -- FUNCOES
+    public void inserir(int valor) throws Exception {
         No no = new No(valor);
         inserirAVL(this.raiz, no);
     }
-
-    public void inserirAVL(No compararNo, No novoNo) {
+    private void inserirAVL(No compararNo, No novoNo) throws Exception {
 
         if (compararNo == null) {
             this.raiz = novoNo;
@@ -31,42 +31,77 @@ public class ArvoreAvl {
                     inserirAVL(compararNo.getDireita(), novoNo); // recussão
                 }
             } else {
-                // valor igual
+                throw new Exception("No já existe na árvore");
             }
         }
     }
 
-    public void ajustarBalanco(No noAtual) {
-        setNoBalanco(noAtual);
-        int balanceamento = noAtual.getBalanco();
+    public void remover(int valor) {
+        removerAVL(this.raiz, valor);
+    }
+    private void removerAVL(No compararNo, int valor) {
+        if (compararNo == null) {
+            return;
 
-        if (balanceamento == -2) {
-
-            if (getNoaltura(noAtual.getEsquerda().getEsquerda()) >= getNoaltura(noAtual.getEsquerda().getDireita())) {
-                noAtual = rotacaoRR(noAtual);
-
-            } else {
-                noAtual = rotacaoLR(noAtual);
-            }
-
-        } else if (balanceamento == 2) {
-
-            if (getNoaltura(noAtual.getDireita().getDireita()) >= getNoaltura(noAtual.getDireita().getEsquerda())) {
-                noAtual = rotacaoLL(noAtual);
-
-            } else {
-                noAtual = rotacaoRL(noAtual);
-            }
-        }
-
-        if (noAtual.getPai() != null) {
-            ajustarBalanco(noAtual.getPai());
         } else {
-            this.raiz = noAtual;
+
+            if (compararNo.getValor() > valor) {
+                removerAVL(compararNo.getEsquerda(), valor);
+
+            } else if (compararNo.getValor() < valor) {
+                removerAVL(compararNo.getDireita(), valor);
+
+            } else if (compararNo.getValor() == valor) {
+                removeNo(compararNo);
+            }
         }
     }
+    private void removeNo(No noAlvo) {
+        No noAux;
+
+        if (noAlvo.getEsquerda() == null || noAlvo.getDireita() == null) {
+
+            if (noAlvo.getPai() == null) {
+                this.raiz = null;
+                noAlvo = null;
+                return;
+            }
+            noAux = noAlvo;
+
+        } else {
+            noAux = sucessor(noAlvo);
+            noAlvo.setValor(noAux.getValor());
+        }
+
+        No noAux2;
+        if (noAux.getEsquerda() != null) {
+            noAux2 = noAux.getEsquerda();
+        } else {
+            noAux2 = noAux.getDireita();
+        }
+
+        if (noAux2 != null) {
+            noAux2.setPai(noAux.getPai());
+        }
+
+        if (noAux.getPai() == null) {
+            this.raiz = noAux2;
+        } else {
+            if (noAux == noAux.getPai().getEsquerda()) {
+                noAux.getPai().setEsquerda(noAux2);
+            } else {
+                noAux.getPai().setDireita(noAux2);
+            }
+            setNoBalanco(noAux.getPai());
+        }
+        noAux = null;
+    }
+    // -- FUNCOES
+
+
+
     // -- ROTAÇÕES
-    public No rotacaoRR(No noAlvo) {
+    private No rotacaoRR(No noAlvo) {
 
         No noEsquerda = noAlvo.getEsquerda();
         noEsquerda.setPai(noAlvo.getPai());
@@ -95,7 +130,7 @@ public class ArvoreAvl {
 
         return noEsquerda;
     }
-    public No rotacaoLL(No noAlvo) {
+    private No rotacaoLL(No noAlvo) {
 
         No noDireita = noAlvo.getDireita(); // pega o filho direito do alvo
         noDireita.setPai(noAlvo.getPai()); // seta o pai do alvo como pai do filho direito
@@ -124,17 +159,46 @@ public class ArvoreAvl {
 
         return noDireita;
     }
-    public No rotacaoLR(No noAlvo) {
+    private No rotacaoLR(No noAlvo) {
         noAlvo.setEsquerda(rotacaoLL(noAlvo.getEsquerda()));
         return rotacaoRR(noAlvo);
     }
-    public No rotacaoRL(No noAlvo) {
+    private No rotacaoRL(No noAlvo) {
         noAlvo.setDireita(rotacaoRR(noAlvo.getDireita()));
         return rotacaoLL(noAlvo);
     }
     // -- ROTAÇÕES
 
     // -- FERRAMENTAS
+    private void ajustarBalanco(No noAtual) {
+        setNoBalanco(noAtual);
+        int balanceamento = noAtual.getBalanco();
+
+        if (balanceamento == -2) {
+
+            if (getNoaltura(noAtual.getEsquerda().getEsquerda()) >= getNoaltura(noAtual.getEsquerda().getDireita())) {
+                noAtual = rotacaoRR(noAtual);
+
+            } else {
+                noAtual = rotacaoLR(noAtual);
+            }
+
+        } else if (balanceamento == 2) {
+
+            if (getNoaltura(noAtual.getDireita().getDireita()) >= getNoaltura(noAtual.getDireita().getEsquerda())) {
+                noAtual = rotacaoLL(noAtual);
+
+            } else {
+                noAtual = rotacaoRL(noAtual);
+            }
+        }
+
+        if (noAtual.getPai() != null) {
+            ajustarBalanco(noAtual.getPai());
+        } else {
+            this.raiz = noAtual;
+        }
+    }
     private void setNoBalanco(No no) {
         no.setBalanco(getNoaltura(no.getDireita()) - getNoaltura(no.getEsquerda()));
     }
@@ -156,6 +220,22 @@ public class ArvoreAvl {
            return 1 + Math.max(getNoaltura(no.getEsquerda()), getNoaltura(no.getDireita()));
         }
 
+    }
+    private No sucessor(No q) {
+        if (q.getDireita() != null) {
+            No r = q.getDireita();
+            while (r.getEsquerda() != null) {
+                r = r.getEsquerda();
+            }
+            return r;
+        } else {
+            No p = q.getPai();
+            while (p != null && q == p.getDireita()) {
+                q = p;
+                p = q.getPai();
+            }
+            return p;
+        }
     }
     // -- FERRAMENTAS
 
